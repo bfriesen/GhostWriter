2c9ed740da7d8244786ca97f65621c70888821e5
Brian Friesen <brian.friesen@gmail.com>
2022-06-24 11:51:08 -0400

    Update solution and project

--- GhostWriter/Program.cs
+++ GhostWriter/Program.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Windows.Forms;
-
 namespace GhostWriter
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            string filePath = null;
+			string filePath = null;
 
             if (args != null && args.Length == 1 && File.Exists(args[0]))
             {
                 filePath = args[0];
             }
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+			
+            ApplicationConfiguration.Initialize();
             Application.Run(new MainForm(filePath));
         }
     }
